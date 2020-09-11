27217b4281961762205852bc579f0beae13e30f3
lempiji <lempiji@gmail.com>
2020-09-12 02:48:21 +0900

    Add example for MNIST

---
+++ examples/mnist/source/app.d
@@ -0,0 +1,135 @@
+import std.stdio;
+import std.file;
+import std.path;
+import std.typecons;
+import std.meta;
+
+import golem;
+
+void main()
+{
+	auto trainImages = cast(ubyte[]) std.file.read(buildNormalizedPath("mnist_data", "train-images-idx3-ubyte"));
+	auto trainLabels = cast(ubyte[]) std.file.read(buildNormalizedPath("mnist_data", "train-labels-idx1-ubyte"));
+	auto testImages = cast(ubyte[]) std.file.read(buildNormalizedPath("mnist_data", "t10k-images-idx3-ubyte"));
+	auto testLabels = cast(ubyte[]) std.file.read(buildNormalizedPath("mnist_data", "t10k-labels-idx1-ubyte"));
+
+	auto trainDataset = makeDataset(trainImages[16 .. $], trainLabels[8 .. $]);
+	auto testDataset = makeDataset(testImages[16 .. $], testLabels[8 .. $]);
+
+	import std.file : exists, read;
+
+	auto model = new Model;
+	if (exists("model.dat"))
+		unpackParameters(cast(ubyte[]) read("model.dat"), model);
+
+	auto optimizer = createOptimizer!SGD(model);
+
+	import std.datetime.stopwatch : StopWatch;
+
+	StopWatch sw;
+	foreach (epoch; 0 .. 5)
+	{
+		sw.reset();
+		sw.start();
+
+		import std.random : randomCover;
+		import std.range : chunks;
+
+		foreach (samples; trainDataset.randomCover().chunks(32))
+		{
+			auto batch = batchTensor(samples);
+			auto input = batch[0];
+			auto label = batch[1];
+
+			auto output = model.forward(input);
+			auto loss = model.loss(output, label);
+
+			optimizer.resetGrads();
+			loss.backward();
+			optimizer.trainStep();
+		}
+
+		auto trainBatch = batchTensor(trainDataset);
+		auto trainLoss = model.loss(model.forward(trainBatch[0]), trainBatch[1]);
+		auto testBatch = batchTensor(testDataset);
+		auto testLoss = model.loss(model.forward(testBatch[0]), testBatch[1]);
+
+		sw.stop();
+		writefln!"trainLoss/testLoss : %f / %f  %s[secs/epoch]"(mean(trainLoss).value[0], mean(testLoss).value[0], sw.peek().total!"seconds");
+
+		import std.file : fwrite = write;
+
+		fwrite("model.dat", packParameters(model));
+	}
+}
+
+
+Tuple!(float[], float[])[] makeDataset(ubyte[] images, ubyte[] labels)
+in(images.length == labels.length * 784)
+{
+	import std.array : appender;
+
+	auto result = appender!(Tuple!(float[], float[])[]);
+
+	float[784] input;
+	float[10] label;
+	label[] = 0;
+
+	enum max = cast(float) ubyte.max;
+	foreach (i; 0 .. labels.length)
+	{
+		input[] = images[i * 784 .. i * 784 + 784][] / max;
+		label[labels[i]] = 1;
+		scope (success) label[labels[i]] = 0;
+
+		result.put(tuple(input.dup, label.dup));
+	}
+
+	return result.data;
+}
+
+Tuple!(Tensor!(float, [0, 28, 28]), Tensor!(float, [0, 10])) batchTensor(R)(R dataset)
+{
+	import std.array : appender;
+	auto input = appender!(float[]);
+	auto label = appender!(float[]);
+
+	foreach (data; dataset)
+	{
+		input ~= data[0];
+		label ~= data[1];
+	}
+
+	auto inputTensor = tensor!([0, 28, 28])(input.data);
+	auto labelTensor = tensor!([0, 10])(label.data);
+
+	return tuple(inputTensor, labelTensor);
+}
+
+
+class Model
+{
+	Linear!(float, 28 * 28, 100) fc1;
+	Linear!(float, 100, 10) fc2;
+
+	alias parameters = AliasSeq!(fc1, fc2);
+
+	this()
+	{
+		foreach (ref p; parameters)
+			p = new typeof(p);
+	}
+
+	auto forward(T)(T x)
+	{
+		auto h1 = relu(fc1(flatten(x)));
+		auto h2 = relu(fc2(h1));
+		return h2;
+	}
+
+	auto loss(T, U)(T output, U label)
+	{
+		auto diff = label - output;
+		return mean(diff * diff);
+	}
+}
