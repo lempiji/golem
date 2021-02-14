f546fc442e732c7aaa5ba79d578c104c613840ca
lempiji <lempiji@gmail.com>
2021-02-15 00:50:56 +0900

    Add golem.nn.batchNorm

--- source/golem/nn.d
+++ source/golem/nn.d
@@ -78,6 +78,134 @@ unittest
     static assert(!canBackward!(typeof(d)));
 }
 
+class BatchNorm(T, size_t[] Shape, UseGradient useGrad = UseGradient.yes)
+{
+	Tensor!(T, Shape, UseGradient.no) mean;
+	Tensor!(T, Shape, UseGradient.no) var;
+	Tensor!(T, Shape, useGrad) factor;
+	Tensor!(T, Shape, useGrad) offset;
+
+    Tensor!(T, Shape, UseGradient.no) tempMean;
+	Tensor!(T, Shape, UseGradient.no) tempVar;
+	Tensor!(T, Shape, UseGradient.no) temps;
+	T momentum = 0.9;
+
+	alias parameters = AliasSeq!(mean, var, factor, offset);
+
+	this()
+	{
+		mean = zeros!(T, Shape);
+		var = zeros!(T, Shape);
+		factor = ones!(T, Shape, useGrad);
+		offset = zeros!(T, Shape, useGrad);
+        tempMean = zeros!(T, Shape);
+		tempVar = zeros!(T, Shape);
+		temps = zeros!(T, Shape);
+	}
+
+	auto opCall(size_t[] ShapeX, UseGradient useGradX)(Tensor!(T, ShapeX, useGradX) x, bool isTrain)
+	{
+		static assert(ShapeX[1 .. $] == Shape);
+
+		import std.math : sqrt;
+        import golem.math : broadcastOp;
+
+		if (isTrain && x.shape[0] != 0)
+		{
+			import mir.math.sum : mirsum = sum;
+			import mir.ndslice : transposed;
+			import golem.util : expandIndex;
+
+            enum eps = 1e-7;
+			immutable batchFactor = T(1.0) / x.shape[0];
+
+            tempMean.value.flattened[] = 0;
+			foreach (t; x.value.ipack!1)
+			{
+                tempMean.value[] += t;
+			}
+            tempMean.value[] *= batchFactor;
+
+			tempVar.value.flattened[] = 0;
+			foreach (t; x.value)
+			{
+				tempVar.value[] += (t[] - tempMean.value[]) ^^ 2;
+			}
+            tempVar.value[] *= batchFactor;
+
+            this.mean.value[] = momentum * this.mean.value[] + (1 - momentum) * tempMean.value[];
+			this.var.value[] = momentum * this.var.value[] + (1 - momentum) * tempVar.value[];
+
+            this.temps.value[] = this.var.value.map!(a => sqrt(a + 1e-7));
+            tempVar.value[] = tempVar.value.map!(a => sqrt(a + 1e-7));
+            return broadcastOp!"+"(broadcastOp!"*"(broadcastOp!"-"(x, this.tempMean), factor / this.tempVar), offset);
+		}
+
+        this.temps.value[] = this.var.value.map!(a => sqrt(a + 1e-7));
+		return broadcastOp!"+"(broadcastOp!"*"(broadcastOp!"-"(x, this.mean), factor / this.temps), offset);
+	}
+}
+
+unittest
+{
+    auto x = tensor!([0, 2, 2])([
+        1.0f, 2.0f, 3.0f, 4.0f,
+        2.0f, 3.0f, 4.0f, 5.0f,
+        3.0f, 4.0f, 5.0f, 6.0f,
+        4.0f, 5.0f, 6.0f, 7.0f,
+    ]);
+
+    auto bn = new BatchNorm!(float, [2, 2]);
+    auto y = bn(x, true);
+
+    import std.math : approxEqual;
+
+    assert(bn.mean.value[0, 0].approxEqual(0.25f));
+    assert(bn.mean.value[0, 1].approxEqual(0.35f));
+    assert(bn.mean.value[1, 0].approxEqual(0.45f));
+    assert(bn.mean.value[1, 1].approxEqual(0.55f));
+    
+    assert(bn.var.value[0, 0].approxEqual(0.125f));
+    assert(bn.var.value[0, 1].approxEqual(0.125f));
+    assert(bn.var.value[1, 0].approxEqual(0.125f));
+    assert(bn.var.value[1, 1].approxEqual(0.125f));
+
+    import std.math : sqrt;
+    import std.conv : text;
+
+    assert(y.value[0, 0, 0].approxEqual((1.0f - 2.5f) / sqrt(1.25f)), text(y.value[0, 0, 0]));
+    assert(y.value[0, 0, 1].approxEqual((2.0f - 3.5f) / sqrt(1.25f)), text(y.value[0, 0, 1]));
+    assert(y.value[0, 1, 0].approxEqual((3.0f - 4.5f) / sqrt(1.25f)), text(y.value[0, 1, 0]));
+    assert(y.value[0, 1, 1].approxEqual((4.0f - 5.5f) / sqrt(1.25f)), text(y.value[0, 1, 1]));
+}
+
+unittest
+{
+    auto x = tensor!([0, 2, 2])([
+        1.0f, 2.0f, 3.0f, 4.0f,
+        2.0f, 3.0f, 4.0f, 5.0f,
+    ]);
+
+    auto bn = new BatchNorm!(float, [2, 2]);
+    auto y = bn(x, true);
+
+    import std.conv : text;
+
+    assert(x.grads[] == [[[0.0f, 0.0f], [0.0f, 0.0f]], [[0.0f, 0.0f], [0.0f, 0.0f]]], text(x.grads));
+    y.backward();
+
+    import std.math : approxEqual;
+
+    assert(x.grads[0, 0, 0].approxEqual(2.0f));
+    assert(x.grads[0, 0, 1].approxEqual(2.0f));
+    assert(x.grads[0, 1, 0].approxEqual(2.0f));
+    assert(x.grads[0, 1, 1].approxEqual(2.0f));
+    assert(x.grads[1, 0, 0].approxEqual(2.0f));
+    assert(x.grads[1, 0, 1].approxEqual(2.0f));
+    assert(x.grads[1, 1, 0].approxEqual(2.0f));
+    assert(x.grads[1, 1, 1].approxEqual(2.0f));
+}
+
 struct Activation(alias f)
 {
     import std.functional : unaryFun;
