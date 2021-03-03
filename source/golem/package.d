f2933e97eccaf3b8f442e0e574ff6204d50c574c
lempiji <lempiji@gmail.com>
2021-03-03 23:58:16 +0900

    Add import golem.trainer

--- source/golem/package.d
+++ source/golem/package.d
@@ -1,9 +1,10 @@
 module golem;
 
+public import golem.data.common;
 public import golem.math;
 public import golem.metrics;
 public import golem.model;
 public import golem.nn;
 public import golem.optimizer;
 public import golem.tensor;
-public import golem.data.common;
+public import golem.trainer;
